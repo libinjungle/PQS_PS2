b02a043e7350bb2b1f2e3674990819bf16962061
libinjungle <libinnku@gmail.com>
2016-03-30 14:45:01 -0400

    Improved thready-safey

--- src/edu/nyu/pqs/stopwatch/impl/StopWatch.java
+++ src/edu/nyu/pqs/stopwatch/impl/StopWatch.java
@@ -1,24 +1,23 @@
 package edu.nyu.pqs.stopwatch.impl;
 
-import edu.nyu.pqs.stopwatch.api.IStopwatch;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Collections;
+
+
+import edu.nyu.pqs.stopwatch.api.IStopwatch;
 
 /**
- * Two states of StopWatch.
+ * Two states of StopWatch. StopWatch can be in RUNNING state or STOPPED state.
  */
 enum State {
   RUNNING, STOPPED;
 }
 
 /**
- * StopWatch object.
- * @@lapTimes  laps that have been taken by StopWatch.
- * @@id        the unique id that specify a StopWatch.
- * @@state     whether StopWatch is running or stopped.
- * @@lock      an object used for synchronization.
- * @@startTime the starting time of a StopWatch.
- * 
+ * Don't list class variables in javadoc unless they are publicly accessible.
+ * This class implements the IStopWatch interface. It can be used to calculate the laps
+ * between starting point until the stop watch is stopped. All methods should be thread-safe.
  * @author BINLI
  *
  */
@@ -26,12 +25,12 @@ public class StopWatch implements IStopwatch  {
   private List<Long> lapTimes;
   private String id;
   private State state;
-  // For synchronization purpose.
+  // This lock object is used for synchronization of threads.
   private Object lock;
   private long startTime;
   
   /**
-   * Constructor for StopWatch. Initialize class fields.
+   * Don't use javadoc for constructor
    */
   public StopWatch(String id) {
     this.id = id;
@@ -42,20 +41,23 @@ public class StopWatch implements IStopwatch  {
   }
   
   /**
+   * "Override annotation should be there. It can avoid mistakes. For example,
+   * using public boolean equals(someclass object) in implemented class instead of 
+   * public boolean equals(Object object) in the interface.
+   * 
    * Returns the Id of this stopwatch
    * @return the Id of this stopwatch.  Will never be empty or null.
    */
+  @Override
   public String getId() {
-    if (this.id == null || this.id.length() == 0) {
-      return null;
-    }
     return this.id;
   }
   
   /**
-   * Starts the stopwatch.
+   * Starts the stopwatch. This method should be thread-safe.
    * @throws IllegalStateException if called when the stopwatch is already running
    */
+  @Override
   public void start() {
     synchronized(lock) {
       if (state == State.RUNNING) {
@@ -68,9 +70,10 @@ public class StopWatch implements IStopwatch  {
 
   /**
    * Stores the time elapsed since the last time lap() was called
-   * or since start() was called if this is the first lap.
+   * or since start() was called if this is the first lap. This method should be thread-safe.
    * @throws IllegalStateException if called when the stopwatch isn't running
    */
+  @Override
   public void lap() {
     synchronized(lock) {
       if (state == State.STOPPED) {
@@ -84,15 +87,22 @@ public class StopWatch implements IStopwatch  {
   }
 
   /**
-   * Stops the stopwatch (and records one final lap).
+   * Stops the stopwatch (and records one final lap). This method should be thread-safe.
    * @throws IllegalStateException if called when the stopwatch isn't running
    */
+  @Override
   public void stop() {
     synchronized(lock) {
       if (state == State.STOPPED) {
         throw new IllegalStateException("The stopwatch already stopped.");
       }
+      // Should use System.nanoTime() since it is much more accurate. 
+      // But it is an expensive call.
       long curTime = System.currentTimeMillis();
+      // start -> stop -> start leaves a lap in lapTimes. This should clear the last lap and 
+      // continue timing where it left off. This will cause an error for adjacent stop/start, 
+      // since the time between stop->start should not be counted.
+      // Use a boolean to indicate the last state is not START.
       long diff = curTime - startTime;
       lapTimes.add(diff);
       state = State.STOPPED;
@@ -101,8 +111,9 @@ public class StopWatch implements IStopwatch  {
 
   /**
    * Resets the stopwatch.  If the stopwatch is running, this method stops the
-   * watch and resets it.  This also clears all recorded laps.
+   * watch and resets it.  This also clears all recorded laps. This method should be thread-safe.
    */
+  @Override
   public void reset() {
     synchronized(lock) {
       if (state == State.RUNNING) {
@@ -112,48 +123,58 @@ public class StopWatch implements IStopwatch  {
       lapTimes.clear();
     }
   }
-
+  
   /**
+   * This methods should be thread-safe. Otherwise, lapTimes size check and return 
+   * may run in parallel.
    * Returns a list of lap times (in milliseconds).  This method can be called at
    * any time and will not throw an exception.
    * @return a list of recorded lap times or an empty list if no times are recorded.
    */
+  @Override
   public List<Long> getLapTimes() {
-    if (lapTimes.size() == 0) {
-      return new LinkedList<Long>();
+    synchronized (lock) {
+      if (lapTimes.size() == 0) {
+        return new LinkedList<Long>();
+      }
+      // getStopwatches() and getLapTimes() return references to private members. 
+      // Would be better to return a copy, so that the user can’t alter internal state or make 
+      // changes that will affect expected execution 
+      // (changing it externally producing ConcurrentModificationExceptions).
+      return Collections.unmodifiableList(lapTimes);
     }
-    return lapTimes;
   }
   
   /**
+   * Thred-safe. The hashcode method in List<StopWatch> iterate the list.
+   * No need to compare state and lapTimes. They are all runtime behavior.
    * Compare to StopWatch object.
    */
   @Override
   public boolean equals(Object object) {
-    if (object == this) {
-      return true;
+    synchronized(lock) {
+      if (object == this) {
+        return true;
+      }
+      if (!(object instanceof StopWatch)) {
+        return false;
+      }
+      StopWatch other = (StopWatch) object;
+      return other.id.equals(this.id);
     }
-    if (!(object instanceof StopWatch)) {
-      return false;
-    }
-    StopWatch other = (StopWatch) object;
-    return other.id.equals(this.id)
-        && other.state.equals(this.state)
-        && other.lapTimes.equals(this.lapTimes);
   }
   
   /**
+   * Thred-safe. The equals methods in List<StopWatch> iterate the list.
    * Return the hashcode for a StopWatch object.
    */
   @Override 
   public int hashCode() {
-    int result = 17;
-    result = 31 * result + this.id.hashCode();
-    result = 31 * result + this.state.hashCode();
-    synchronized(lock) {
-      result = 31 * result + this.lapTimes.hashCode();
+    synchronized (lock) {
+      int result = 17;
+      result = 31 * result + this.id.hashCode();
+      return result;
     }
-    return result;
   }
   
   /**
@@ -162,7 +183,9 @@ public class StopWatch implements IStopwatch  {
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    sb.append("StopWatch: id = " + this.id);
+    // 
+    sb.append("StopWatch: id = ");
+    sb.append(this.id);
     if (state == State.RUNNING) {
       sb.append(", The state is RUNNING. ");
     } else {
--- src/edu/nyu/pqs/stopwatch/impl/StopwatchFactory.java
+++ src/edu/nyu/pqs/stopwatch/impl/StopwatchFactory.java
@@ -1,9 +1,10 @@
 package edu.nyu.pqs.stopwatch.impl;
 
-import java.util.List;
-import java.util.LinkedList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.Collections;
+
 import edu.nyu.pqs.stopwatch.api.IStopwatch;
 
 /**
@@ -13,43 +14,49 @@ import edu.nyu.pqs.stopwatch.api.IStopwatch;
  * 
  */
 public class StopwatchFactory {
-    private static List<IStopwatch> watchList = new LinkedList<>();
-    // Check if an id has been taken.
-    private static Set<String> idSet = new HashSet<>();
-    private static Object locker = new Object();
-    
-	/**
-	 * Creates and returns a new IStopwatch object
-	 * @param id The identifier of the new object
-	 * @return The new IStopwatch object
-	 * @throws IllegalArgumentException if <code>id</code> is empty, null, or already
-     * taken.
-	 */
-	public static IStopwatch getStopwatch(String id) {
-	  synchronized(locker) {
-	    if (id == null || id.length() == 0) {
-	        throw new IllegalArgumentException("The id is invalid!");
-	      }
-	      if (idSet.contains(id)) {
-	        throw new IllegalArgumentException("This id is already taken!");
-	      }
-	      IStopwatch stopWatch = new StopWatch(id);
-	      watchList.add(stopWatch);
-	      idSet.add(id);
-	      return stopWatch;
-	  }
-	}
+  // Instead of using two data structures(one List and another HashSet), using one Map
+  // mapping id to StopWatch. 
+  private static Map<String, IStopwatch> watchMap = new HashMap<>();
+  // Check if an id has been taken.
+  // private static Set<String> idSet = new HashSet<>();
+  private static Object locker = new Object();
 
-	/**
-	 * Returns a list of all created stopwatches
-	 * @return a List of al creates IStopwatch objects.  Returns an empty
-	 * list if no IStopwatches have been created.
-	 */
-	public static List<IStopwatch> getStopwatches() {
-		if (watchList.size() == 0) {
-		  return new LinkedList<IStopwatch>();
-		}
-		return watchList;
-	}
+  /**
+   * Creates and returns a new IStopwatch object
+   * 
+   * @param id
+   *          The identifier of the new object
+   * @return The new IStopwatch object
+   * @throws IllegalArgumentException
+   *           if <code>id</code> is empty, null, or already taken.
+   */
+  public static IStopwatch getStopwatch(String id) {
+    synchronized (locker) {
+      if (id == null || id.length() == 0) {
+        throw new IllegalArgumentException("The id is invalid!");
+      }
+      if (watchMap.containsKey(id)) {
+        throw new IllegalArgumentException("This id is already taken!");
+      }
+      IStopwatch stopWatch = new StopWatch(id);
+      watchMap.put(id, stopWatch);
+      return stopWatch;
+    }
+  }
+
+  /**
+   * This methods should be thread-safe, since size check and return may run in
+   * parallel. Returns a list of all created stopwatches
+   * 
+   * @return a List of al creates IStopwatch objects. Returns an empty list if
+   *         no IStopwatches have been created.
+   */
+  public static Map<String, IStopwatch> getStopwatches() {
+    synchronized (locker) {
+      if (watchMap.size() == 0) {
+        return new HashMap<String, IStopwatch>();
+      }
+      return Collections.unmodifiableMap(watchMap);
+    }
+  }
 }
-    
